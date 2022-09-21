388d7c011e4e3ef3bd92b091c803cb33aef17be1
gulci <gulci@LAPTOP-KPNR185M>
2022-09-21 16:49:26 +0300

    commit

---
+++ src/variables/main.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class main {
+
+	public static void main(String[] args) {
+		//reusability:tekrar tekrar kullanmak,bir kere yaz her yerde kullan
+		//değişkenler sayesinde bellek tasarrufu sağlarız
+		int ogrenciSayisi = 10;
+		String mesaj = "ögrenci sayisi:";
+		System.out.println("öğrenci sayisi : "+ ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+
+	}
+
+}
